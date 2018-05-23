34f4fa77f3ec2537ca985650dc1e7cbffb9c024e
schneiderl <casdpa@gmail.com>
2018-05-23 13:08:24 -0300

    fixed lint errors

--- jiraUploader.py
+++ jiraUploader.py
@@ -20,40 +20,41 @@ logging.basicConfig(level=logging.INFO)
 _app = Flask(__name__)
 _api = Api(_app)
 CORS(_app)
-warnings.filterwarnings("ignore") #just during prototype phase
+warnings.filterwarnings("ignore")  # just during prototype phase
 port = int(os.getenv("PORT", 9099))
 global _authenticatedHeader
 global _data
 
+
 @_app.route('/api/createtasks', methods=['POST', 'OPTIONS'])
 def _parse_request():
-	
-	if request.method=='OPTIONS':
-		return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
-	#file = request.files['file']
-	
-	r = _get_auth_from_request()[1]
-	if(r.status_code==200):
-		global _authenticatedHeader
-		_authenticatedHeader = _authenticate_header(r)
-		read = request.data.decode('utf-8')
-		
-		lines = read.splitlines() #no good
-		for row in lines[1:]:
-			logging.info(row)
-			r = _post_issue(row)
-			logging.info(r)
-	else:
-		abort(403)
 
-	return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+    if request.method == 'OPTIONS':
+        return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+    #file = request.files['file']
+
+    r = _get_auth_from_request()[1]
+    if(r.status_code == 200):
+        global _authenticatedHeader
+        _authenticatedHeader = _authenticate_header(r)
+        read = request.data.decode('utf-8')
+
+        lines = read.splitlines()  # no good
+        for row in lines[1:]:
+            logging.info(row)
+            r = _post_issue(row)
+            logging.info(r)
+    else:
+        abort(403)
+
+    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+
 
 def _get_auth_from_request():
-	username = request.authorization['username']
-	password = request.authorization['password']
-	return _post_auth(username, password)
+    username = request.authorization['username']
+    password = request.authorization['password']
+    return _post_auth(username, password)
 
 
 if __name__ == '__main__':
-    _app.run(host='0.0.0.0', port=port)	
-
+    _app.run(host='0.0.0.0', port=port)
